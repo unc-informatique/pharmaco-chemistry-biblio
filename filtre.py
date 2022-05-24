ffa9267e828b6a5adc17300c9f130be6ac1a1f2c
rémi colin <remicolin2707@gmail.com>
2022-05-25 05:55:53 +1100

    add chart and use json for load chart

--- filtre.py
+++ filtre.py
@@ -1,7 +1,7 @@
 #%%
 import pandas as pd
 from pandas import DataFrame
-
+import numpy as np
 
 
 def row(dataset, seuil) -> DataFrame:
@@ -63,6 +63,75 @@ def keep_with_or_without(dataset, value) -> DataFrame:
     return dataset.loc[:, :, value].T.loc[:, :, value].T
 
 
+# def intersection_metric(arr):
+#     """The trivial one. Just keep the bottom right cell. Amount to compute on "the old matrix" """
+#     [FF, FT], [TF, TT] = arr.reshape(2, 2)
+#     return TT / (FF + FT + TF + TT)
+
+
+def tt_projection_metric(arr):
+    """The trivial one. Just keep the bottom right cell. Amount to compute on "the old matrix" """
+    [FF, FT], [TF, TT] = arr.reshape(2, 2)
+    return TT
+
+
+def row_implication_metric(arr):
+    """The % of paper that have both keywords among the first one. Values add to 1 row wise"""
+    [FF, FT], [TF, TT] = arr.reshape(2, 2)
+    return TT / (TF + TT)
+
+
+def col_implication_metric(arr):
+    """The % of paper that have both keywords among the second one. Values add to 1 col wise"""
+    [FF, FT], [TF, TT] = arr.reshape(2, 2)
+    return TT / (FT + TT)
+
+
+def fowlkes_mallows_metric(arr):
+    """Computes the Fowlkes-Mallows index: sqrt(the product of row and col implication)"""
+    [FF, FT], [TF, TT] = arr.reshape(2, 2)
+    return TT / np.sqrt((FT + TT) * (TF + TT))
+
+
+def accuracy_metric(arr):
+    """Computes the accuracy"""
+    [FF, FT], [TF, TT] = arr.reshape(2, 2)
+    return (TT + FF) / (FF + FT + TF + TT)
+
+
+def x_metric(arr):
+    """Some another one by our own"""
+    [FF, FT], [TF, TT] = arr.reshape(2, 2)
+    return (FF + TT - FT - TF) / (FF + FT + TF + TT)
+
+
+def fraction_metric(arr):
+    """The % of paper that have both keywords among the ones having at least one."""
+    [FF, FT], [TF, TT] = arr.reshape(2, 2)
+    return TT / (FT + TF - TT)
+
+
+def odds_metric(arr):
+    """The odds of having related keywords."""
+    [FF, FT], [TF, TT] = arr.reshape(2, 2)
+    return (FF * TT + 0.5) / (FT * TF + 0.5)
+
+
+def logodds_metric(arr):
+    """The log odds of having related keywords."""
+    return np.log(odds_metric(arr))
+
+
+metrics = [
+    tt_projection_metric,
+    row_implication_metric,
+    col_implication_metric,
+    fowlkes_mallows_metric,
+    fraction_metric,
+    odds_metric,
+    accuracy_metric,
+        # logodds_metric,
+]
 
 
 
---
+++ index.js
@@ -0,0 +1,46 @@
+const main = document.querySelector('#main');
+const jsonName = ["mark.json", "vbar.json", "hit_map.json", "sankey.json"];
+const select = document.querySelector('#select');
+
+
+/**
+ * ajoute tout les nom dans jsonName a la select box 
+ */
+function addToSelect(){
+    jsonName.forEach(function(name) {
+        const option = document.createElement('option');
+        option.value = name;
+        option.innerHTML = name;
+        select.appendChild(option);
+    });
+}
+
+
+/**
+ * fonction qui crée le graphique dans la div 
+ * @param {url du json} json 
+ */
+function addChart(json) {
+    var spec = json;
+    var embedOpt = {"mode": "vega-lite"};
+
+    function showError(el, error){
+        el.innerHTML = ('<div class="error" style="color:red;">'
+                        + '<p>JavaScript Error: ' + error.message + '</p>'
+                        + "<p>This usually means there's a typo in your chart specification. "
+                        + "See the javascript console for the full traceback.</p>"
+                        + '</div>');
+        throw error;
+    }
+    vegaEmbed("#content-chart", spec, embedOpt)
+      .catch(error => showError(el, error));
+}
+
+
+select.addEventListener('change', (event) => {
+    addChart("http://127.0.0.1:5500/json_chart/" + event.target.value);
+});
+
+addToSelect();
+addChart("http://127.0.0.1:5500/json_chart/" + jsonName[0]);
+

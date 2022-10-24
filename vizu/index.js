1f9d4cf21a37379b422ead91f3b1d204fe3df926
Romuald THION <romuald.thion@univ-nc.nc>
2022-10-24 17:03:16 +1100

    path

--- vizu/index.js
+++ vizu/index.js
@@ -1,50 +1,51 @@
-const main = document.querySelector('#main');
+const main = document.querySelector("#main");
 const jsonName = ["vbar.json", "mark.json", "sankey_template.json"];
 
 /**
- * ajoute tout les nom dans jsonName a la select box 
+ * ajoute tout les nom dans jsonName a la select box
  */
-function addToSelect(){
-    jsonName.forEach(function(name) {
-        addChart(name, "/json_chart/" + name);
-    });
+function addToSelect() {
+  jsonName.forEach(function (name) {
+    addChart(name, "json_chart/" + name);
+  });
 }
-
 
 /**
- * fonction qui crée le graphique dans la div 
- * @param {url du json} json 
+ * fonction qui crée le graphique dans la div
+ * @param {url du json} json
  */
 function addChart(name, json) {
-    var spec = json;
-    var embedOpt = {"mode": "vega-lite"};
+  var spec = json;
+  var embedOpt = { mode: "vega-lite" };
 
-    var div = document.createElement('li');
-    var br = document.createElement('br');
-    function showError(el, error){
-        div.innerHTML = ('<div class="error" style="color:red;">'
-                        + '<p>JavaScript Error: ' + error.message + '</p>'
-                        + "<p>This usually means there's a typo in your chart specification. "
-                        + "See the javascript console for the full traceback.</p>"
-                        + '</div>');
-        throw error;
-    }
-    // create a new div 
-    
-    // add the id name to the div but remove the .json
-    div.classList.add("chart");
-    name = name.replace(".json", "");
-    div.id = name;
-    // add the div to the main div
-    document.querySelector("#content-chart").appendChild(br);
-    document.querySelector("#content-chart").appendChild(div);
+  var div = document.createElement("li");
+  var br = document.createElement("br");
+  function showError(el, error) {
+    div.innerHTML =
+      '<div class="error" style="color:red;">' +
+      "<p>JavaScript Error: " +
+      error.message +
+      "</p>" +
+      "<p>This usually means there's a typo in your chart specification. " +
+      "See the javascript console for the full traceback.</p>" +
+      "</div>";
+    throw error;
+  }
+  // create a new div
 
-    vegaEmbed("#"+name, spec, embedOpt)
-      .catch(error => showError(div, error));
+  // add the id name to the div but remove the .json
+  div.classList.add("chart");
+  name = name.replace(".json", "");
+  div.id = name;
+  // add the div to the main div
+  document.querySelector("#content-chart").appendChild(br);
+  document.querySelector("#content-chart").appendChild(div);
+
+  vegaEmbed("#" + name, spec, embedOpt).catch((error) => showError(div, error));
 }
 
+console.info(`vega.version = ${vega.version}`);
+console.info(`vegaLite.version = ${vegaLite.version}`);
+console.info(`vegaEmbed.version = ${vegaEmbed.version}`);
 
 addToSelect();
-
-
-
